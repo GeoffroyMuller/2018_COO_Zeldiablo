35ddf261c5e41c17439ac59517d1d819391c22d9
Theo Biancalana <theobiancalana@gmail.com>
2018-05-29 09:50:11 +0200

    labyrinthe v1

--- projet_zeldiablo/src/Labyrinthe.java
+++ projet_zeldiablo/src/Labyrinthe.java
@@ -1,4 +1,14 @@
 
 public class Labyrinthe {
+	
+	private Case[][] grille;
+	
+	public Labyrinthe(){
+		grille = new Case[50][50];
+		grille[0][24] = new Entree();
+		
+		grille[49][24] = new Entree();
+	}
+	
 
 }
