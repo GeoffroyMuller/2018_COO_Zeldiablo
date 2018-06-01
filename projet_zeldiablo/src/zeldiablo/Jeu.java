5383fff973dd64b71e1e9d93a005e6c723c33e75
Geoff M <geoffroym2@gmail.com>
2018-06-01 08:38:04 +0200

    ajout class jeu

---
+++ projet_zeldiablo/src/zeldiablo/Jeu.java
@@ -0,0 +1,12 @@
+package zeldiablo;
+
+import java.util.ArrayList;
+
+public class Jeu {
+	private Aventurier av;
+	private ArrayList<Labyrinthe> listLab;
+	public Jeu() {
+		av = new Aventurier();
+	}
+
+}
