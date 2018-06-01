9954d989dc53482bccec817135a6cb2a3debe1ba
Theo Biancalana <theobiancalana@gmail.com>
2018-06-01 09:29:26 +0200

    pricipaletest

--- projet_zeldiablo/src/zeldiablo/Jeu.java
+++ projet_zeldiablo/src/zeldiablo/Jeu.java
@@ -8,6 +8,10 @@ public class Jeu {
 	private ArrayList<Labyrinthe> listLab;
 	public Jeu() {
 		av = new Aventurier();
+		for (int i = 0; i < 10; i++) {
+			listLab.add(new Labyrinthe());
+		}
 	}
+	
 
 }
---
+++ projet_zeldiablo/src/zeldiablo/PrincipaleTest.java
@@ -0,0 +1,10 @@
+package zeldiablo;
+
+public class PrincipaleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
