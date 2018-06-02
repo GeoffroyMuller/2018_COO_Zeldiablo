eac7030b8a36e08c0a65e59e2776776543f826a0
Loic Masson <loic.masson1@etu.univ-lorraine.fr>
2018-06-02 14:51:18 +0100

    Ajout des tests de la méthode déplacement

--- projet_zeldiablo/src/test/TestDeplacement.java
+++ projet_zeldiablo/src/test/TestDeplacement.java
@@ -10,15 +10,93 @@ import zeldiablo.Labyrinthe;
 
 public class TestDeplacement {
 	@Test
-	public void testDeplacerEnDehors() {
+	public void testDeplacerEnDehorsDroiteGauche() {
 		//initialisation des donnees
 		Labyrinthe l = new Labyrinthe();
 		Coordonnee c = new Coordonnee(1,1);
 		Coordonnee c1 = new Coordonnee(0,1);
+		Aventurier a = new Aventurier(c,l); 
+		//Methode testee
+		a.deplacerAventurier(c1);
+		//assertion 
+		assertFalse("Le joueur ne devrait pas pouvoir se deplacer en dehors", a.deplacerAventurier(c1));
+	}
+	
+	@Test
+	public void testDeplacerEnDehorsHautBas() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		Coordonnee c = new Coordonnee(1,1);
+		Coordonnee c1 = new Coordonnee(1,0);
 		Aventurier a = new Aventurier(c,l);
 		//Methode testee
 		a.deplacerAventurier(c1);
 		//assertion
-		assertFalse("Le joueur ne devrait pas pouvoir se deplacer", a.deplacerAventurier(c1));
+		assertFalse("Le joueur ne devrait pas pouvoir se deplacer en dehors", a.deplacerAventurier(c1));
+	}
+	
+	@Test
+	public void testDeplacerEnDehorsLimiteLaby() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		int y = l.getGrille().length;
+		Coordonnee c = new Coordonnee(1,49);
+		Coordonnee c1 = new Coordonnee(1,y);
+		Aventurier a = new Aventurier(c,l);
+		//Methode testee
+		a.deplacerAventurier(c1); 
+		//assertion
+		assertFalse("Le joueur ne devrait pas pouvoir se deplacer en dehors", a.deplacerAventurier(c1));
+	}
+	
+	@Test
+	public void testDeplacerEnDehorsLimiteLaby2() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		int y = l.getGrille().length;
+		Coordonnee c = new Coordonnee(49,1);
+		Coordonnee c1 = new Coordonnee(y,1);
+		Aventurier a = new Aventurier(c,l);
+		//Methode testee
+		a.deplacerAventurier(c1); 
+		//assertion
+		assertFalse("Le joueur ne devrait pas pouvoir se deplacer en dehors", a.deplacerAventurier(c1));
+	}
+	 
+	@Test
+	public void testDeplacerValide() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		Coordonnee c = new Coordonnee(1,1);
+		Coordonnee c1 = new Coordonnee(1,2);
+		Aventurier a = new Aventurier(c,l);
+		//Methode testee
+		a.deplacerAventurier(c1);
+		//assertion
+		assertTrue("Le joueur ne devrait pouvoir se deplacer", a.deplacerAventurier(c1));
+	}
+	
+	@Test
+	public void testDeplacerCaseTraversable() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		Coordonnee c = new Coordonnee(1,48);
+		Coordonnee c1 = new Coordonnee(1,47);
+		//Methode testee
+		l.getGrille()[c1.getX()][c1.getY()].estTraversable();
+		//assertion
+		assertTrue("Le joueur devrait pouvoir se deplacer sur la case", l.getGrille()[c.getX()][c.getY()].estTraversable());
+	}
+	
+	@Test
+	public void testDeplacerCaseNonTraversable() {
+		//initialisation des donnees
+		Labyrinthe l = new Labyrinthe();
+		Coordonnee c = new Coordonnee(1,1);
+		Coordonnee c1 = new Coordonnee(0,1);
+		//Methode testee
+		l.getGrille()[c1.getX()][c1.getY()].estTraversable();
+		//assertion
+		assertFalse("Le joueur ne devrait pas pouvoir se deplacer sur la case", l.getGrille()[c1.getX()][c1.getY()].estTraversable());
 	}
 }
