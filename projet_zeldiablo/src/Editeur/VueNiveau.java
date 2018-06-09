737a91b24bd323686b49f9f6dc437eb1b0bdb7b5
Geoffroy Muller <johndoe@example.comgeoffroym2@gmail.com>
2018-06-09 19:42:50 +0200

    editeur: modif affichage

--- projet_zeldiablo/src/Editeur/VueNiveau.java
+++ projet_zeldiablo/src/Editeur/VueNiveau.java
@@ -5,25 +5,45 @@ import java.util.*;
 
 import javax.swing.*;
 
+import zeldiablo.ZeldiabloDessin;
+
 public class VueNiveau  extends JPanel implements Observer{
 	
+	private int x=0;
+	private int y=0;
+	private int taillecase=ZeldiabloDessin.TAILLE;
+	
 	private Niveau niv;
+	
 	public VueNiveau(Niveau nive) {
 		// TODO Auto-generated constructor stub
 		setBackground(new Color(180,180,180));
 		niv = nive;
+		niv.addObserver(this);
 	}
 	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		x=10;
+		y=10;
 		g.setColor(Color.black);
-		g.fillRect(10, 10, 10, 10);
+		if(niv.getTab_case()!=null) {
+			g.setColor(Color.BLACK);
+			for(int i=0;i<niv.getTaille();i++) {
+				for(int j=0;j<niv.getTaille();j++) {
+					g.fillRect(x, y, taillecase, taillecase);
+					x+=taillecase+2;
+				}
+				y+=taillecase+2;
+				x=10;
+			}
+		}
 	}
 
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
-		
+		repaint();
 	}
 
 }
